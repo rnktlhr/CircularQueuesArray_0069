f23065d52fac867df1d2e11d09a790a69d5b20f0
rnktlhr <rinakitluhur10@gmail.com>
2024-05-31 14:39:32 +0700

    menambahkan if dan else dalam function insert

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -17,10 +17,25 @@ class Queues{
             cin >> num;
             cout << endl;
 
+            //cek apakah antrian penuh 
             if ((FRONT == 0 && REAR == max - 1)|| (FRONT == REAR +1)){
                 cout << "\nQueue overflow\n";
                 return;
             }
+
+            //cek apakah antrian kosong
+            if(FRONT == -1){
+                FRONT = 0;
+                REAR = 0;
+            }
+
+            else {
+                if (REAR == max - 1)
+                    REAR = 0;
+                else 
+                    REAR = REAR + 1;
+            }
+            queue_array[REAR] = num;
         }
 };
 
