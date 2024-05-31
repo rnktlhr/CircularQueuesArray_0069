f3ccd3dc5f036c5f0d51ba330810191e5ed9094e
rnktlhr <rinakitluhur10@gmail.com>
2024-05-31 15:16:46 +0700

    menambahkan main

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -100,5 +100,45 @@ class Queues{
 };
 
 int main(){
+    Queues q;
+    char ch;
 
+    while(true){
+        try{
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice" << endl;
+            cin >> ch;
+            cout << endl;
+
+            switch(ch){
+                case '1': {
+                    q.insert();
+                    break;
+                }
+                case '2': {
+                    q.remove();
+                    break;
+                }
+                case '3' : {
+                    q.display();
+                    break;
+                }
+                case '4' : {
+                    return 0;
+                }
+                default: {
+                    cout << "Indvalid option!!" << endl;
+                    break;
+                }
+            }
+        }
+        catch(exception& e){
+            cout << "Check for the vlues entered." << endl;
+        }
+    }
+    return 0;
 }
