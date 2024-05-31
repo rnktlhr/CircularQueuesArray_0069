9b85457bde6a423a1f95b2c5083fa5af3bd2c866
rnktlhr <rinakitluhur10@gmail.com>
2024-05-31 15:04:58 +0700

    menambahkan looping while didalam else yang berada di function void display

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -85,6 +85,16 @@ class Queues{
                     cout << queue_array[FRONT_position] << "  ";
                     FRONT_position++;
                 }
+
+                FRONT_position = 0;
+                
+                //iterasi dari awal array hingga REAR
+                while(FRONT_position <= REAR_position){
+                    cout << queue_array[FRONT_position]<< "  ";
+                    FRONT_position++;
+                }
+
+                cout << endl;
             }
         }
 };
