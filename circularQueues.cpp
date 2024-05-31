26f3bf3cdd9e678c18965f749a07f6c4c872f7a6
rnktlhr <rinakitluhur10@gmail.com>
2024-05-31 15:18:27 +0700

    menambahkan kekurangan

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -110,7 +110,7 @@ int main(){
             cout << "2. Implement delete operation" << endl;
             cout << "3. Display values" << endl;
             cout << "4. Exit" << endl;
-            cout << "Enter your choice" << endl;
+            cout << "Enter your choice (1-4)" << endl;
             cin >> ch;
             cout << endl;
 
